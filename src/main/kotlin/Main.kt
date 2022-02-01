42580512624c8eb3748b74974087792599da12d0
Jonry Simbolon <jonrysimbolon19@gmail.com>
2022-02-01 23:00:08 +0700

    Second Commit in Berhitung branch

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,3 +2,7 @@ fun main(args: Array<String>) {
     println("Hello World!")
     println("Program arguments: ${args.joinToString()}")
 }
+
+fun Berhitungg(a: Int, b: Int): Int {
+    return a + b
+}
--- src/test/kotlin/BerhitungTest.kt
+++ src/test/kotlin/BerhitungTest.kt
@@ -1,9 +1,13 @@
 import org.junit.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class BerhitungTest {
 
     @Test
     fun testTambah(){
-
+        val c = Berhitungg(2,3)
+        assertEquals(5, c,"Benar")
     }
 }
